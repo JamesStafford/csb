014a82e7d297710972fd7506659e0f742b31b6a5
James Stafford <jstafford@bomgar.com>
2017-12-10 18:05:11 -0600

    Added implemented pseudo-code from tip.

--- main.cpp
+++ main.cpp
@@ -1,32 +1,27 @@
 #include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
 
 using namespace std;
 
 int main()
 {
-
-    while (1) {
+    while (true) {
         int x;
         int y;
-        int nextCheckpointX; // x position of the next check point
-        int nextCheckpointY; // y position of the next check point
-        int nextCheckpointDist; // distance to the next checkpoint
-        int nextCheckpointAngle; // angle between your pod orientation and the direction of the next checkpoint
+        int nextCheckpointX;
+        int nextCheckpointY;
+        int nextCheckpointDist;
+        int nextCheckpointAngle;
         cin >> x >> y >> nextCheckpointX >> nextCheckpointY >> nextCheckpointDist >> nextCheckpointAngle; cin.ignore();
+
         int opponentX;
         int opponentY;
         cin >> opponentX >> opponentY; cin.ignore();
 
-        // Write an action using cout. DON'T FORGET THE "<< endl"
-        // To debug: cerr << "Debug messages..." << endl;
+        int thrust = 100;
+        if (nextCheckpointAngle > 90 || nextCheckpointAngle < -90) {
+            thrust = 0;
+        }
 
-
-        // You have to output the target position
-        // followed by the power (0 <= thrust <= 100)
-        // i.e.: "x y thrust"
-        cout << nextCheckpointX << " " << nextCheckpointY << " 80" << endl;
+        cout << nextCheckpointX << " " << nextCheckpointY << " " << thrust << endl;
     }
 }
